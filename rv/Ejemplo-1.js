d9c5c095406c90107e0185cd0ced842a00c910a7
toro1031 <toro_1031@hotmail.com>
2016-08-16 15:33:05 -0500

    Create Ejemplo-1.js

---
+++ rv/Ejemplo-1.js
@@ -0,0 +1,14 @@
+  var miCanvas1 = document.getElementById("miCanvas1");
+  var contexto1 = miCanvas1.getContext("2d");
+  contexto1.fillStyle = "red";
+  contexto1.fillRect(30, 30, 50, 50);
+  
+  var miCanvas2 = document.getElementById("miCanvas2");
+  var contexto2 = miCanvas2.getContext("2d");
+  contexto2.fillStyle = "green";
+  contexto2.fillRect(30, 30, 50, 50);
+  
+  var miCanvas3 = document.getElementById("miCanvas3");
+  var contexto3 = miCanvas3.getContext("2d");
+  contexto3.fillStyle = "blue";
+  contexto3.fillRect(30, 30, 50, 50);
