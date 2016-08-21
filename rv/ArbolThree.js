b72edc8261d97b0dc7a4065804e8bd4fabed85c0
toro1031 <toro_1031@hotmail.com>
2016-08-21 00:54:33 -0500

    Update ArbolThree.js

--- rv/ArbolThree.js
+++ rv/ArbolThree.js
@@ -8,18 +8,18 @@ renderer.setSize( window.innerWidth*.95, window.innerHeight*.95 );
 document.body.appendChild( renderer.domElement );
 
 var geometry = new THREE.CylinderGeometry( 3, 3, 50, 30, 5 );
-var material = new THREE.MeshBasicMaterial( { color: 0xC53FE6 } );
+var material = new THREE.MeshBasicMaterial( { color: 0x7A4B0D } );
 var cylinder = new THREE.Mesh( geometry, material );
 cylinder.rotateX(Math.PI/4);
-cylinder.rotateY(Math.PI/4);
+cylinder.rotateY(-Math.PI/4);
 scene.add( cylinder );
 
-var geometry2 = new THREE.SphereGeometry( 20, 25, 25 );
-var material2 = new THREE.MeshBasicMaterial( { color: 0x00FF00 } );
+var geometry2 = new THREE.SphereGeometry( 8, 25, 25 );
+var material2 = new THREE.MeshBasicMaterial( { color: 0x32720D } );
 var sphere = new THREE.Mesh( geometry2, material2 );
 scene.add( sphere );
 
-sphere.position.x = 35;
+sphere.position.y = 35;
 
 renderer.render( scene, camera );
 
