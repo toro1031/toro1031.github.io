dc12426d1ca9232001caf19b0071e911ba01befc
toro1031 <toro_1031@hotmail.com>
2016-08-21 00:15:26 -0500

    Update ArbolThree.js

--- rv/ArbolThree.js
+++ rv/ArbolThree.js
@@ -1,21 +1,22 @@
-var escena = new THREE.Scene();
-var camara = new THREE.PerspectiveCamera();
-camara.position.z = 10;
-var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
-document.body.appendChild( renderizador.domElement );
-var forma = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshNormalMaterial();
-var cubo = new THREE.Mesh( forma, material );
-escena.add( cubo );
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
 
 var render = function () {
-	requestAnimationFrame( render );
-	cube.rotation.x += 0.1;
-	cube.rotation.y += 0.1;
-	renderizador.render(escena, camara);
-	};
-	
+requestAnimationFrame( render );
+cube.rotation.x += 0.1;
+cube.rotation.y += 0.1;
+renderer.render(scene, camera);
+};
+
 render();
-
-
