4916027d1d855a34e56ed368347f9a7ec479f4a9
toro1031 <toro_1031@hotmail.com>
2016-08-21 01:01:56 -0500

    Update ArbolThree.js

--- rv/ArbolThree.js
+++ rv/ArbolThree.js
@@ -1,5 +1,5 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 50 );
 
 camera.position.z = 100;
 
@@ -12,7 +12,7 @@ var material = new THREE.MeshBasicMaterial( { color: 0x7A4B0D } );
 var cylinder = new THREE.Mesh( geometry, material );
 scene.add( cylinder );
 
-var geometry2 = new THREE.SphereGeometry( 8, 50, 10 );
+var geometry2 = new THREE.SphereGeometry( 8, 50, 50 );
 var material2 = new THREE.MeshBasicMaterial( { color: 0x32720D } );
 var sphere = new THREE.Mesh( geometry2, material2 );
 scene.add( sphere );
