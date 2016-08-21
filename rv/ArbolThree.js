c568a5fa2c0c8372919f371ce9d0bc8cc8b68cb4
toro1031 <toro_1031@hotmail.com>
2016-08-21 00:12:18 -0500

    Update ArbolThree.js

--- rv/ArbolThree.js
+++ rv/ArbolThree.js
@@ -1,13 +1,21 @@
 var escena = new THREE.Scene();
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 100;
+camara.position.z = 10;
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
 document.body.appendChild( renderizador.domElement );
 var forma = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshNormalMaterial();
 var cubo = new THREE.Mesh( forma, material );
-cubo.rotateX(-Math.PI/4);
-cubo.rotateY(Math.PI/4);
 escena.add( cubo );
-renderizador.render( escena, camara );
+var render = function () {
+				requestAnimationFrame( render );
+
+				cube.rotation.x += 0.1;
+				cube.rotation.y += 0.1;
+
+				renderizador.render(escena, camara);
+			};
+render()
+
+
