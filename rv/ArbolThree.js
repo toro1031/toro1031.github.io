9692c04b32503834aedc77e8681c81300905b1e3
toro1031 <toro_1031@hotmail.com>
2016-08-21 00:21:02 -0500

    Update ArbolThree.js

--- rv/ArbolThree.js
+++ rv/ArbolThree.js
@@ -10,7 +10,7 @@ var material = new THREE.MeshBasicMaterial( { color: 0xC53FE6 } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 50;
+camera.position.z = 5;
 
 var render = function () {
 requestAnimationFrame( render );
