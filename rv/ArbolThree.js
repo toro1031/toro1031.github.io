72ce893d61446c0f42f448682763dd38ba81198e
toro1031 <toro_1031@hotmail.com>
2016-08-21 01:30:04 -0500

    Update ArbolThree.js

--- rv/ArbolThree.js
+++ rv/ArbolThree.js
@@ -35,23 +35,3 @@ cone.position.x = 50;
 cone.position.y = 13;
 
 renderer.render( scene, camera );
-
-function animate() {
-
-				requestAnimationFrame( animate, renderer.domElement );
-
-				render();
-				stats.update();
-
-			}
-function render() {
-
-				theta += 0.1;
-
-				camera.position.x = 100 * Math.sin( THREE.Math.degToRad( theta ) );
-				camera.lookAt( scene.position );
-
-				camera.updateMatrixWorld();
-}
-
-animate();
