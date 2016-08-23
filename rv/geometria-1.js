0311fcdc3899d2a422f28169623b800dec32ce6e
toro1031 <toro_1031@hotmail.com>
2016-08-23 15:51:45 -0500

    Update geometria-1.js

--- rv/geometria-1.js
+++ rv/geometria-1.js
@@ -26,7 +26,7 @@ escena.add( malla );
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 5;
 
-var renderizador = new THREE.WebGLRender();
+var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
 document.body.appendChild( renderizador.domElement );
 renderizador.render( escena, camara );
