0304a4c8c3f0b69137928950e0161beae26e6851
toro1031 <toro_1031@hotmail.com>
2016-09-06 14:58:13 -0500

    Update camaraproyeccion.js

--- rv/camaraproyeccion.js
+++ rv/camaraproyeccion.js
@@ -3,7 +3,7 @@ var relacionAspecto = window.innerWidth / window.innerHeight;
 var planoCercano = 1;
 var planoLejano = 1000;
 
-var camara = new THREE.PerspectiveCamera( campoVision, realcionAspecto, planoCercano, planoLejano);
+var camara = new THREE.PerspectiveCamera( campoVision, relacionAspecto, planoCercano, planoLejano);
 camara.position.z = 15;
 
 var cubo = new THREE.Mesh( new THREE.BoxGeometry( 2, 2, 2 ), new THREE.MeshNormalMaterial() );
