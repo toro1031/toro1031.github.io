e358febcc89dfff7716e8293ab5685f8c63e48a0
toro1031 <toro_1031@hotmail.com>
2016-09-08 15:24:24 -0500

    Update Ejercicio-Tablero.js

--- rv/Ejercicio-Tablero.js
+++ rv/Ejercicio-Tablero.js
@@ -65,7 +65,7 @@ var camara = new THREE.PerspectiveCamera( campoVision, relacionAspecto, planoCer
 camara.position.z = 200;
 camara.position.y = 80;
 camara.position.x = 40;
-camara.lookAt = (40, 0, 40);
+camara.lookAt = (40, -40, 40);
 
 renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(  window.innerWidth*.95, window.innerHeight*.95 );
