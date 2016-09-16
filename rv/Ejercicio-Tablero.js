0b8f4b7b7d59eab1affe9949eee7dc89749ba774
toro1031 <toro_1031@hotmail.com>
2016-09-16 01:13:56 -0500

    Update Ejercicio-Tablero.js

--- rv/Ejercicio-Tablero.js
+++ rv/Ejercicio-Tablero.js
@@ -88,7 +88,8 @@ for (var i = 0; i < 8; i++) {
 var marco1 = new THREE.BoxGeometry( 100, 5, 5);
 var cafe = new THREE.MeshBasicMaterial( { color: 0x653909 } );
 var marcomalla1 = new THREE.Mesh(marco1, cafe);
-marcomalla1.position.x=40;
+marcomalla1.position.x=30;
+marcomalla1.position.z=85;
 escena.add(marcomalla1);
 
 //Camara
