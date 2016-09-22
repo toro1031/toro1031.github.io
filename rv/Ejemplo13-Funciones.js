52d72d28593dda5d93ed1a086fa588643a99c953
toro1031 <toro_1031@hotmail.com>
2016-09-22 15:03:36 -0500

    Update Ejemplo13-Funciones.js

--- rv/Ejemplo13-Funciones.js
+++ rv/Ejemplo13-Funciones.js
@@ -5,9 +5,14 @@ escena.add(malla);
 camara = new THREE.PerspectiveCamera();
 renderizador = new THREE.WebGLRender();
 renderizador.setSize(700,700);
+document.body.appendChild(renderizador.domElement);
 }
-
 
 var main = function(p){
 renderizador.render(escena, camara);
 }
+
+var escena, camara, renderizador;
+
+init(1);
+main(2);
