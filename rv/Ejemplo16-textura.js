b749c1c20a5f13b9d5f4a5f29d2c25b615219812
toro1031 <toro_1031@hotmail.com>
2016-10-04 14:45:09 -0500

    Create Ejemplo16-textura.js

---
+++ rv/Ejemplo16-textura.js
@@ -0,0 +1,35 @@
+var TEXTURA = new Object();
+
+TEXTURA.retrollamada = function( textura ) {
+  var material = new THREE.MeshBasicMaterial( {map: textura} );
+  TEXTURA.malla = new THREE.Mesh( new THREE.SphereGeometry( 1 ), material );
+  
+  TEXTURA.escena.add(TEXTURA.malla);
+}
+
+TEXTURA.setup = function() {
+  TEXTURA.escena = new THREE.Scene();
+  var cargador = new THREE.TextureLoader();
+  cargador.load("earth_atmos_2048.jpg", TEXTURA.retrollamada);
+  
+  TEXTURA.camara = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  
+  TEXTURA.camara.position.z = 5;
+  
+  var lienzo = document.getElementById("Ejemplo16-textura");
+  TEXTURA.renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
+  
+  TEXTURA.renderizador.setSize( 600, 600 );
+}
+
+TEXTURA.loop = function() {
+  requestAnimationFrame( TEXTURA.loop);
+  if ( TEXTURA.malla !== undefined ) {
+    TEXTURA.malla.rotateX( 0.01);
+    TEXTURA.malla.rotateY( 0.01);
+  }
+  TEXTURA.renderizador.render( TEXTURA.escena, TEXTURA.camara );
+}
+
+TEXTURA.setup();
+TEXTURA.loop();
