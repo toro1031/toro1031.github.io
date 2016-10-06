98f67178a7857c307fb08759f70fcc101739197f
toro1031 <toro_1031@hotmail.com>
2016-10-06 15:21:18 -0500

    Update Torre-Textura.js

--- rv/Torre-Textura.js
+++ rv/Torre-Textura.js
@@ -71,10 +71,10 @@ TEXTURA.renderizador.setSize( 600, 600 );
 
 TEXTURA.loop = function() {
   requestAnimationFrame( TEXTURA.loop);
- // if ( TEXTURA.malla !== undefined ) {
-   // TEXTURA.malla.rotateX( 0.01);
+  if ( TEXTURA.malla !== undefined ) {
+    TEXTURA.malla.rotateX( 0.01);
     //TEXTURA.malla.rotateY( 0.01);
- // }
+  }
   TEXTURA.renderizador.render( TEXTURA.escena, TEXTURA.camara );
 }
 
