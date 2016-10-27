54e4d2d132f75644d2538a94f002140ec3ca594d
toro1031 <toro_1031@hotmail.com>
2016-10-27 15:56:30 -0500

    Update Robot.js

--- rv/Robot.js
+++ rv/Robot.js
@@ -50,7 +50,7 @@ Environment.prototype.setMap = function(map){
   for( var i = 0; i < map.length ; i++)
   for( var j = 0; j < map.length ; j++) {
     if (map[i][j] === "x")
-    this.add( new Wall( l, j - _offset, -(i-_offset)));
+    this.add( new Wall( 1, j - _offset, -(i-_offset)));
     else if (map[i][j] === "r")
     this.add( new Robot( 0.5, j-_offset, -(i-_offset)));
   }
