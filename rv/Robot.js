7a2dd6f2baed215d0e5cf185b9986a808feb9ade
toro1031 <toro_1031@hotmail.com>
2016-10-27 15:45:10 -0500

    Update Robot.js

--- rv/Robot.js
+++ rv/Robot.js
@@ -52,9 +52,76 @@ Environment.prototype.setMap = function(map){
     if (map[i][j] === "x")
     this.add( new Wall( l, j - _offset, -(i-_offset)));
     else if (map[i][j] === "r")
-    this.add( new Robot(0.5, j-_offset, -(i-_offset)));
+    this.add( new Robot( 0.5, j-_offset, -(i-_offset)));
   }
 }
+
+function Sensor(position, direction){
+  THREE.Raycaster.call(this, position, direction);
+  this.colision = false;
+}
+Sensor.prototype = new THREE.Raycaster();
+
+function Robot(size,x,y){
+  Agent.call(this, x, y);
+  
+  this.sensor = new Sensor();
+  this.actuator = new THREE.Mesh( new THREE.BosxGeometry( size, size, size ), new THREE.MeshBasicMaterial({color: '#aa0000'}));
+  this.actuator.commands = [];
+  this.add(this.actuator);
+}
+
+Robot.prototye = new Agent();
+
+Robot.prototype.sense = function(environment){
+  this.sensor.set( this.position, new THREE.Vector3( Math.cos(this.rotation.z), Math.sin(this.rotation.z), 0));
+  var obstaculo = this.sensor.intersectObjects(environment.children.true);
+  if ((obstaculo.length > 0 && (obstaculo[0].distancia <= .5)))
+    this.sensor.colision = true;
+  else
+    this.sensor.colision = false;
+};
+
+Robot.prototype.plan = function(environment){
+  this.actuator.commands[];
+  
+  if (this.sensor.colision === true)
+    this.actuator.commands.push('rotateCCW');                               
+  else
+    this.actuator.commands.push('goStraight');
+};
+
+Robot.prototype.act = function(environment){
+  var command = this.actuator.commands.pop();
+  
+  if (command === undefined)
+    console.log('Undefined command');
+  else if(command in this.operations)
+    this.operations[command](this);
+  else
+    console.log('Unknown command');
+};
+
+Robot.prototype.operations = {};
+
+Robot.protoype.operations.goStraight = function(robot, distance){
+ if (distance === undefined)
+  distance = .05;
+  robot.position.x += distance*Math.cos(robot.rotation.z);
+  robot.position.y += distance*Math.sin(robot.rotation.z);
+};
+
+Robot.prototype.operations.rotateCW = function(robot, angle){
+  if (angle === undefined)
+    angle = -Math.PI/2;
+    robot.rotation.z += angle;
+};
+
+Robot.prototype.operations.rotateCCW = function(robot, angle){
+  if (angle === undefined)
+    angle = Math.PI/2;
+    robot.rotation.z += angle;
+};
 
 function setup() {
   var mapa = new Array();
